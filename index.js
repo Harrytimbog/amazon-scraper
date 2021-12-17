f1338e1dc29aad902bb04424e0702138a88872a9
Timilehin Arigbede <Harriemannie@gmail.com>
2021-12-17 12:52:10 +0100

    Create End point for product info

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = "59ef84be287bba26357f5519b0058332";
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send(`Welcome to Ecommerce Scraper API.`)
 });
 
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
